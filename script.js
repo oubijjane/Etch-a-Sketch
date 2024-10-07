395c6439e7239ea1e29a92b236bf1c2beeb17adf
oubijjane <79411385+oubijjane@users.noreply.github.com>
2024-10-07 12:57:35 +0100

    add the ability to change the color opacity
    after each interaction the color opacity get
    stronger til it get full opacity and resete to 0.1

--- script.js
+++ script.js
@@ -1,18 +1,68 @@
 const div = document.querySelector(".container");
-let size = 20;
+const div2 = document.querySelector(".container1");
+const body = document.querySelector("body");
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+let color = ["rgb(255, 0, 0,",
+    "rgb(255, 165, 0,",
+    "rgb(255, 255, 0,",
+    "rgb(0, 128, 0,",
+    "rgb(0, 0, 255,",
+    "rgb(75, 0, 130,",
+    "rgb(238, 130, 238,"];
+let size = 10;
+let op = 0.11;
 let dem = 600;
 div.style.width = dem + "px";
-div.style.height = "600PX";
-
-for (i = 0; i < size*size; i++) {
-    const cell = document.createElement("div");
-    cell.className = "cell";
-    cell.style.width = "16px";
-   // cell.style.height = "16px";
-    //cell.textContent = "hi";
-    div.appendChild(cell);
+div.style.height = dem + "px";
+function getRandomNUmber() {
+    const minCeiled = Math.ceil(0);
+    const maxFloored = Math.floor(color.length);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
 }
+
+function createGrid(size) {
+    if (size > 100) {
+        size = 100;
+    }
+    let result = dem / size - 2;
+    for (i = 0; i < size * size; i++) {
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cell.style.width = result + "px";
+        cell.style.height = result + "px";
+        //cell.style.opacity = op + "%";
+        div.appendChild(cell);
+    }
+}
+createGrid(size);
 const cell = document.querySelector(".cell");
 div.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "red";
+    if (e.target.classList.contains("cell")) {
+        e.target.style.backgroundColor = "rgba(255, 0, 0," + op + ")";
+        op += 0.10;
+        console.log(op);
+        if (op > 1) {
+            op = 0.11;
+        }
+    }
+})
+
+div2.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    switch (e.target.id) {
+        case "reset":
+            div.querySelectorAll(".cell").forEach((cell) => {
+                cell.style.backgroundColor = "white";
+
+            })
+            break;
+        case "size":
+            size = input.value;
+            div.querySelectorAll(".cell").forEach((cell) => {
+                cell.remove();
+            })
+            createGrid(size);
+    }
+
 })
